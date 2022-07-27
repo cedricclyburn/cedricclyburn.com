c3d1678e606338e85e8664498872d500b18ff25a
Cedric Clyburn <cedricclyburn@gmail.com>
2022-07-26 23:38:51 -0400

    added bio

--- data/authors/default.md
+++ data/authors/default.md
@@ -7,6 +7,6 @@ company: MongoDB
 twitter: https://twitter.com/cedricclyburn
 linkedin: https://www.linkedin.com/in/cedricclyburn
 github: https://github.com/cedricclyburn
-text1: Hi there! My name is Cedric, I'm Cedric, a passionate software developer and continual learner of all things cloud-native! I live in Raleigh, NC, and love running, backpacking, and traveling.
+text1: Hi there! My name is Cedric, I'm Cedric, a passionate software developer and continual learner of all things cloud-native. I live in Raleigh, NC, and love running, backpacking, and traveling.
 text2: I'm a big fan of Kubernetes, DevOps, and using technology to make an impact.
 ---
--- data/experienceData.js
+++ data/experienceData.js
@@ -1,28 +1,54 @@
 const experienceData = [
   {
-    title: 'Software Developer',
-    company: 'Maul',
-    location: 'Reykjavik, Iceland',
-    range: 'October 2020 - present',
-    url: 'https://maul.is',
-    text1: 'Maintain and update software for food orders to multiple companies',
+    title: 'Developer Advocate Intern',
+    company: 'MongoDB',
+    location: 'New York, United States',
+    range: 'June 2022 - present',
+    url: 'https://mongodb.com/',
+    text1: 'Creating technical content and demos for MongoDB and partner technologies including Kubernetes and Tableau, as well as accompanying video content',
     text2:
-      'Work with a variety of different technologies, such as, JavaScript, React, Tailwind CSS, React-Query, Xstate, Dynamo DB, AWS services to name a few',
+      'Aiding in managing DevRel team projects using Jira to keep in check with timelines and other departments',
     text3:
-      'Handle a diverse spectrum of projects on a daily basis, such as helping with daily operations, customer service and maintaining the websites',
+      'Producing weekly episodes for the MongoDB Podcast for a total audience of over 10k, including interviewing, editing, and marketing on Twitter & LinkedIn',
   },
   {
-    title: 'Software Developer',
-    company: 'Reboot hack',
-    location: 'Reykjavik, Iceland',
-    range: 'August 2019 - March 2020',
-    url: 'https://reboothack.is',
+    title: 'Developer Advocate Intern',
+    company: 'Red Hat',
+    location: 'Raleigh, North Carolina, United States',
+    range: 'May 2020 - May 2022',
+    url: 'https://www.redhat.com',
     text1:
-      'Worked in a team of three developers to build and maintain a website for Reboot Hack 2020, the first ever student organized hackathon in Iceland',
+      'Overhauled 25+ client-facing demos and associated documentation for Java, Python, and Node.js tracks in conjunction with the OpenShift developer experience team.',
     text2:
-      'Website held information about the event along side sign up form. Built with Node JS, React and Boostrap. PostgreSQL database',
+      'Developed video content, articles, and multimedia for Red Hat partners and internal developer audiences, resulting in over 100,000 impressions on Youtube, Twitter, and Twitch.',
     text3:
-      'Helped doing multiple things for the event, such as, advertisement, organize IT and more',
+      'Aided in the multi-department launch of KubernetesByExample through the development of 7 Kubernetes training courses.',
+  },
+  {
+    title: 'Product Management Intern',
+    company: 'nCino',
+    location: 'Raleigh, North Carolina, United States',
+    range: 'Jan 2022 - Apr 2022',
+    url: 'https://www.ncino.com/',
+    text1:
+      'Worked to update various epics and stories using Jira and Miro boards.',
+    text2:
+      'Developed a process to move hundreds of accessibility audits from Accessible Web to Jira, streamlining the work and creating documentation for future work.',
+    text3:
+      "Standardized over 6 internal team's Jira Plans views to allow for better planning and initiative view, as well as helping implement auto-scheduling capabilities for sprints.",
+  },
+  {
+    title: 'Software Development Intern',
+    company: 'LiRA',
+    location: 'Chapel Hill, North Carolina, United States',
+    range: 'May 2021 - Jan 2022',
+    url: 'https://liraglobal.com/',
+    text1:
+      'Implemented and tested a data cleaning pipeline using Python, C#, and Azure for removing empty data collected through our LipTrain program to be used for a machine learning algorithm.',
+    text2:
+      'Participated in the VentureWell & Verizon E-Team/Propel program to complete customer discovery, business development, and earned $40k of non-dilutive funding.',
+    text3:
+      "Selected to present for the Verizon & CGI U Final Pitch Showcase to judges Chelsea Clinton, Nicki Palmer, and Phil Weilerstein of the Clinton Foundation.",
   },
 ]
 
