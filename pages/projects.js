152d4bcb51249469f13d3f185a0c82d817e9c463
Cedric Clyburn <cedricclyburn@gmail.com>
2022-07-26 22:59:05 -0400

    small modifications

--- pages/projects.js
+++ pages/projects.js
@@ -13,7 +13,7 @@ export default function Projects() {
             Projects
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            Some of the projects I've build or been a part of.
+            Some of the projects I've worked on or been a part of.
           </p>
         </div>
         <div className="container py-12">
