a79c0107d7686c0f5621b3cecb4f65237a59084c
Cedric Clyburn <cedricclyburn@gmail.com>
2024-04-08 18:08:12 +0000

    Finished home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,9 @@ import {
   LinkedInIcon,
   XIcon,
 } from '@/components/SocialIcons'
-import logoAirbnb from '@/images/logos/airbnb.svg'
-import logoFacebook from '@/images/logos/facebook.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
-import logoStarbucks from '@/images/logos/starbucks.svg'
+import logoMongodb from '@/images/logos/mongodb.svg'
+import logoRedhat from '@/images/logos/redhat.svg'
+import logoNcino from '@/images/logos/ncino.svg'
 import image1 from '@/images/photos/image-1.jpg'
 import image2 from '@/images/photos/image-2.jpg'
 import image3 from '@/images/photos/image-3.jpg'
@@ -187,35 +186,35 @@ function Role({ role }: { role: Role }) {
 function Resume() {
   let resume: Array<Role> = [
     {
-      company: 'Planetaria',
-      title: 'CEO',
-      logo: logoPlanetaria,
-      start: '2019',
+      company: 'Red Hat',
+      title: 'Senior Developer Advocate',
+      logo: logoRedhat,
+      start: '2022',
       end: {
         label: 'Present',
         dateTime: new Date().getFullYear().toString(),
       },
     },
     {
-      company: 'Airbnb',
-      title: 'Product Designer',
-      logo: logoAirbnb,
-      start: '2014',
-      end: '2019',
+      company: 'MongoDB',
+      title: 'Developer Advocate Intern',
+      logo: logoMongodb,
+      start: 'Summer',
+      end: '2022',
     },
     {
-      company: 'Facebook',
-      title: 'iOS Software Engineer',
-      logo: logoFacebook,
-      start: '2011',
-      end: '2014',
+      company: 'Red Hat',
+      title: 'Developer Advocate Intern',
+      logo: logoRedhat,
+      start: '2020',
+      end: '2022',
     },
     {
-      company: 'Starbucks',
-      title: 'Shift Supervisor',
-      logo: logoStarbucks,
-      start: '2008',
-      end: '2011',
+      company: 'nCino',
+      title: 'Product Management Intern',
+      logo: logoNcino,
+      start: 'Semester',
+      end: '2021',
     },
   ]
 
@@ -230,7 +229,7 @@ function Resume() {
           <Role key={roleIndex} role={role} />
         ))}
       </ol>
-      <Button href="#" variant="secondary" className="group mt-6 w-full">
+      <Button href="https://drive.google.com/file/d/161zDRW20ka8ond946g-O6jA7Ftpk-E9Y/view?usp=sharing" variant="secondary" className="group mt-6 w-full">
         Download CV
         <ArrowDownIcon className="h-4 w-4 stroke-zinc-400 transition group-active:stroke-zinc-600 dark:group-hover:stroke-zinc-50 dark:group-active:stroke-zinc-50" />
       </Button>
